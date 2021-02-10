578f4c11dc6ded0d734e2326e3361fce0394f548
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-02-10 20:34:04 -0300

    10-02-2021

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat House - Panel</title>
-<?php
+<?
     // N = RELE N = ACAO
     // 1 = On  |  0 = Off
     if($_POST['11']) {

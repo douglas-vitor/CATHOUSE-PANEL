181c9bd831cb474a890e2eaf66c7af87c5e46d76
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-02-11 20:50:29 -0300

    11-02-2021

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat House - Panel</title>
-<?
-    // N = RELE N = ACAO
-    // 1 = On  |  0 = Off
-    if($_POST['11']) {
-        shell_exec('gpio write 0 0')
-    } elseif($_POST['10']) {
-        shell_exec('gpio write 1 0')
+<?php
+    // N = RELE(GPIO Name) N = ACAO
+    // 1 = Off  |  0 = On
+
+    //RELE 01
+    if($_POST['10'] == "10") {
+        shell_exec('gpio write 0 0');
+    } elseif($_POST['11'] == "11") {
+        shell_exec('gpio write 0 1');
     }
 ?>
 </head>
@@ -24,8 +26,8 @@
     <form method="POST">
         <section class="rele1">
         <span>Rele 01</span>
-        <button name="11">On</button>
-        <button name="10">Off</button>
+        <button name="10" value="10">On</button>
+        <button name="11" value="11">Off</button>
         </section>
         </form>
     </main>
